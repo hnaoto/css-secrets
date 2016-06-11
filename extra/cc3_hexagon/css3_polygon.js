55ec3816e3f382f8aa75680fa1135c8965ba1481
hnaoto <hnaoto@me.com>
2016-06-10 22:46:17 -0400

    random fix on polygon

--- extra/cc3_hexagon/css3_polygon.js
+++ extra/cc3_hexagon/css3_polygon.js
@@ -15,12 +15,7 @@
 	var data =  [{"Hero":"abathur","ID":"'6' ","Free":false},{"Hero":"anubarak","ID":"'1000' ","Free":false},{"Hero":"artanis","ID":"'1639' ","Free":false},{"Hero":"arthas","ID":"'23' ","Free":false},{"Hero":"azmodan","ID":"'1001' ","Free":false},{"Hero":"brightwing","ID":"'392' ","Free":false},{"Hero":"chen","ID":"'963' ","Free":true},{"Hero":"cho","ID":"'1707' ","Free":false},{"Hero":"chromie","ID":"'2037' ","Free":false},{"Hero":"dehaka","ID":"'1952' ","Free":false},{"Hero":"diablo","ID":"'20' ","Free":true},{"Hero":"etc","ID":"'19' ","Free":false},{"Hero":"falstad","ID":"'10' ","Free":false},{"Hero":"gall","ID":"'1735' ","Free":false},{"Hero":"gazlowe","ID":"'3' ","Free":false},{"Hero":"greymane","ID":"'1808' ","Free":false},{"Hero":"illidan","ID":"'9' ","Free":false},{"Hero":"jaina","ID":"'1101' ","Free":false},{"Hero":"johanna","ID":"'1392' ","Free":false},{"Hero":"kaelthas","ID":"'1324' ","Free":false},{"Hero":"kerrigan","ID":"'11' ","Free":false},{"Hero":"kharazim","ID":"'1536' ","Free":false},{"Hero":"leoric","ID":"'1472' ","Free":false},{"Hero":"li-li","ID":"'672' ","Free":false},{"Hero":"li-ming","ID":"'1860' ","Free":false},{"Hero":"lt-morales","ID":"'1676' ","Free":true},{"Hero":"lunara","ID":"'1774' ","Free":false},{"Hero":"malfurion","ID":"'15' ","Free":true},{"Hero":"muradin","ID":"'21' ","Free":false},{"Hero":"murky","ID":"'708' ","Free":false},{"Hero":"nazeebo","ID":"'2' ","Free":false},{"Hero":"nova","ID":"'13' ","Free":false},{"Hero":"raynor","ID":"'1' ","Free":false},{"Hero":"rehgar","ID":"'879' ","Free":false},{"Hero":"rexxar","ID":"'1565' ","Free":true},{"Hero":"sgt-hammer","ID":"'16' ","Free":true},{"Hero":"sonya","ID":"'12' ","Free":false},{"Hero":"stitches","ID":"'22' ","Free":false},{"Hero":"sylvanas","ID":"'1276' ","Free":false},{"Hero":"tassadar","ID":"'7' ","Free":false},{"Hero":"the-butcher","ID":"'1437' ","Free":false},{"Hero":"the-lost-vikings","ID":"'1209' ","Free":false},{"Hero":"thrall","ID":"'1143' ","Free":true},{"Hero":"tracer","ID":"'1993' ","Free":false},{"Hero":"tychus","ID":"'8' ","Free":false},{"Hero":"tyrael","ID":"'18' ","Free":false},{"Hero":"tyrande","ID":"'4' ","Free":false},{"Hero":"uther","ID":"'5' ","Free":false},{"Hero":"valla","ID":"'14' ","Free":true},{"Hero":"xul","ID":"'1892' ","Free":true},{"Hero":"zagara","ID":"'791' ","Free":false},{"Hero":"zeratul","ID":"'17' ","Free":true}];
 	
 	var n = (data.length / 13) * 2 ; 
-	
-	console.log(n);
-	
-	
-	
-	
+
 	
 	for (var i = 0; i < n;  i++) {
 		
@@ -39,9 +34,6 @@
 				var boxF = $('<div class="boxF">  </div>');
 				var boxS = $('<div class="boxS">   </div>');
 				var boxT = $('<div class="boxT"> </div>');
-
-		
-	
 
 				var icon_url = 'http://www.heroesfire.com/images/wikibase/icon/heroes/' + data[j + 7 * i ]['Hero'] + '.png';
 				boxT.attr('style', 'background-image:url(' +  icon_url +  ')' );
